f79583f851041fb246763a3b99ef343a58840488
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2024-11-22 21:45:45 +0530

    .

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,13 @@ import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [CardComponent],
-  exports: [],
+  exports: [CardComponent],
 })
+
+// so here we are telling angular that whereever this sharedModule is used
+// So that I'm declaring it and I'm then telling Angular
+// that it should be available to any other modules
+// that use the shared module.
+
+
 export class SharedModule {}

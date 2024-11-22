cde2c6e66f981de3e5fa8f8f1018a22a2b172272
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2024-11-22 21:45:11 +0530

    Creating & Using Shared Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent], //entry point of application main node
-  imports: [BrowserModule, FormsModule], // we need to tell what componets we are using
+  imports: [BrowserModule, FormsModule, SharedModule], // we need to tell what componets we are using
     // here the imports is used to get all the default functionalities of Angular 
     // to be used into all the components. 
 })
